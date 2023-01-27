ea07796d867dc159ac0492ca9b65601236e3789a
Tenmayos <tenmayos_2@hotmail.com>
2023-01-27 09:07:12 +0300

    -uncommented routing code in `App.js` -Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import './styles/App.css';
 import Home from "./pages/Home";
-/*import Navbar from "./components/Navbar"
 import { Route, Routes } from "react-router-dom";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
-import ContactMe from "./pages/ContactMe";*/
+import ContactMe from "./pages/ContactMe";
 
 function App() {
   return (
     <>
-    {/*}
-    <div className="container">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<ContactMe />} />
       </Routes>
-    </div>
-    {*/}
-    <Home />
     </>
   );
 }
