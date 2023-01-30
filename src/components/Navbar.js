da569ace3d39deaeefaab4f5a3418dc2765b5a51
Tenmayos <tenmayos_2@hotmail.com>
2023-01-30 23:22:40 +0300

    Modified navbar to not render in the root index

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,16 @@
 import { Link } from "react-router-dom"
 import "../styles/App.css"
 import LinksList from "./LinksList"
+import { useLocation } from "react-router-dom"
 
 export default function Navbar() {
+
+    const location = useLocation();
+    
+    if (location.pathname === "/") {
+        return null;
+    }
+    
     return (
         <nav className="navigation">
             <Link to="/" className="site-title">
