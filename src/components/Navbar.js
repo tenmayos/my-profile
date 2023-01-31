7cf2c70f840391015d5d751cc8f546f420b40e54
Tenmayos <tenmayos_2@hotmail.com>
2023-02-01 02:42:37 +0300

    -Finalized the `navbar` -Added `Navbar` related CSS into `App.css`

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom"
 import "../styles/App.css"
 import LinksList from "./LinksList"
 import { useLocation } from "react-router-dom"
@@ -6,17 +5,14 @@ import { useLocation } from "react-router-dom"
 export default function Navbar() {
 
     const location = useLocation();
-    
+
     if (location.pathname === "/") {
         return null;
     }
-    
+
     return (
-        <nav className="navigation">
-            <Link to="/" className="site-title">
-                Bassam
-            </Link>
-            <LinksList />
-        </nav>
+        <div className="topnav">
+            <LinksList showIndex />
+        </div>
     )
 }
