ab5a01af1ffdca620964e06ed5f80c26565d8dd5
Tenmayos <tenmayos_2@hotmail.com>
2023-02-03 23:05:22 +0300

    Moved `About.js` page contents into components

---
+++ src/components/AboutSections/OtherSkillsSection.js
@@ -0,0 +1,28 @@
+export default function OtherSkillsSection() {
+    return (
+        <section className="about-section dark-blue-bg">
+            <h1 className="about-header">Other Skills</h1>
+            <p>Getting right to the point, here is yet another list of the things I can do</p>
+            <ul>
+                <li>
+                    Assemble computer hardware
+                </li>
+                <li>
+                    Git and version control
+                </li>
+                <li>
+                    Docker
+                </li>
+                <li>
+                    Bash & PowerShell
+                </li>
+                <li>
+                    Ubuntu OS
+                </li>
+            </ul>
+            <p>During my path it was rather natural for me to pick up the more modern skills that go with software development,
+                in order to both make my life easier and speed up my development pace.</p>
+            <p>As for the Assembly of computer hardware, this was just me and my curiosity.</p>
+        </section>
+    );
+}
