db0f4f1c22a49f473ff6c1edc0c071297c6e85a3
Tenmayos <tenmayos_2@hotmail.com>
2023-02-03 23:08:43 +0300

    populated `About.js` with new section components

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,26 @@
+import BackgroundSection from "../components/AboutSections/BackgroundSection";
+import EducationSection from "../components/AboutSections/EducationSection";
+import MainSkillsSection from "../components/AboutSections/MainSkillsSection";
+import OtherSkillsSection from "../components/AboutSections/OtherSkillsSection";
+import "../styles/About.css";
+
 export default function About() {
-    return <h1>About</h1>
+
+    return (
+        <>
+            <section className="intro-section">
+                <h1 className="about-header">
+                    I'm Bassam Saleh
+                </h1>
+                <h3 className="about-desc">
+                    Programmer and Video games enthusiast
+                </h3>
+
+            </section>
+            <BackgroundSection />
+            <EducationSection />
+            <MainSkillsSection />
+            <OtherSkillsSection />
+        </>
+    );
 }
