61dfa30eddc11758dc21adeb75acae5e73d26175
Tenmayos <tenmayos_2@hotmail.com>
2023-02-08 17:24:46 +0300

    Reduced the text in `BackgroundSection.js`

--- src/components/AboutSections/BackgroundSection.js
+++ src/components/AboutSections/BackgroundSection.js
@@ -10,20 +10,12 @@ export default function BackgroundSection() {
             <p>
                 I started using computers and playing video games at a very young age.</p>
             <p>Ever since then it has been a childhood dream for me to create my own game.</p>
-            <p>
-                And that is where it all began, where the source of my motivation was born, and through it I started learning programming.
-                Starting with C# only to find out that there is much more where that came from. Right after C# and high school I started looking into other languages and programming paradigms.
-                I tried to learn whatever my eyes fell upon, like I'm in a supermarket and the goods are just all good. However, because I was teaching myself,
-                the resources and the progress I was making didn't blend well with my liking. Thus I considered a different approach of learning one thing that can be applied to all,
-                and that turned out to be problem solving!
-                It is the foundational skill that everything else just builds upon, and thus it was my target.
-                After that learning new programming languages was only a matter of syntax and my growing pace sped up tremendously.
-                Right after that I decided to teach some of my interested friends in college.
-                Satisfied with the results, I decided to do what anyone else would do.
-                I would take those skills to the real world and use them to solve actual problems. Whether they were my own or of those close to me.
-                Building small WPF apps to keep track of store inventories and such was one of the things that I did. But of course,
-                I had to link my skills to the gaming world somehow, so I built a few game hacks/trainers but we shouldn't talk about those here!
+            <p>And that is where it all began, where the source of my motivation was born, and through it I started learning programming. Starting with C#
+                only to find out that there is much more where that came from.
             </p>
+            <p>Leading me to believe that when utilized correctly, logic and software can seem like magic!</p>
+            <p>And this is what drives me on, to help people build the web or desktop apps of their dreams, moving them on forward!</p>
+            <p><strong>I'm Bassam Saleh</strong>, and I will stop at nothing to build your projects or solve any technical issues.</p>
 
         </section>
     );
