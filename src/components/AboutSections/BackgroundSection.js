4d1e4d50fef4f2dc1c3a25b32f22b8357ec861c1
Tenmayos <tenmayos_2@hotmail.com>
2023-02-08 18:00:54 +0300

    Wrapped the paragraph in a `div` for font class

--- src/components/AboutSections/BackgroundSection.js
+++ src/components/AboutSections/BackgroundSection.js
@@ -6,17 +6,18 @@ export default function BackgroundSection() {
             <h1 className="section-header">
                 Background
             </h1>
-            <p>Born in Egypt and mostly raised there despite having a Saudi Arabian nationality.</p>
-            <p>
-                I started using computers and playing video games at a very young age.</p>
-            <p>Ever since then it has been a childhood dream for me to create my own game.</p>
-            <p>And that is where it all began, where the source of my motivation was born, and through it I started learning programming. Starting with C#
-                only to find out that there is much more where that came from.
-            </p>
-            <p>Leading me to believe that when utilized correctly, logic and software can seem like magic!</p>
-            <p>And this is what drives me on, to help people build the web or desktop apps of their dreams, moving them on forward!</p>
-            <p><strong>I'm Bassam Saleh</strong>, and I will stop at nothing to build your projects or solve any technical issues.</p>
-
+            <div className="about-desc">
+                <p>Born in Egypt and mostly raised there despite having a Saudi Arabian nationality.</p>
+                <p>
+                    I started using computers and playing video games at a very young age.</p>
+                <p>Ever since then it has been a childhood dream for me to create my own game.</p>
+                <p>And that is where it all began, where the source of my motivation was born, and through it I started learning programming.
+                    Starting with <strong style={{ "color": "#96e2f0" }}>C#</strong> only to find out that there is much more where that came from.
+                </p>
+                <p>Leading me to believe that when utilized correctly, logic and software can seem like magic!</p>
+                <p>And this is what drives me on, to help people build the web or desktop apps of their dreams, moving them on forward!</p>
+                <p><strong>I'm Bassam Saleh</strong>, and I will stop at nothing to build your projects or solve any technical issues.</p>
+            </div>
         </section>
     );
 }
