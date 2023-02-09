f89546286ab5772451054cb0c160990ed644e0c0
Tenmayos <tenmayos_2@hotmail.com>
2023-02-10 01:57:04 +0300

    Replaced `li`Text with `OuterLink` wrapped the description parts in a `div` Beautified some text parts

--- src/components/AboutSections/MainSkillsSection.js
+++ src/components/AboutSections/MainSkillsSection.js
@@ -5,37 +5,45 @@ export default function MainSkillsSection() {
     return (
         <section className="about-section black-bg">
             <h1 className="section-header">Main Tech Skills</h1>
-            <p>Hmm, where should I begin…</p>
-            <p>I'm proficient in C# and the .NET framework (Obviously) but what would I be if it was all I know, eh?</p>
-            <p>Well, here is a list of the other technologies I know how to use:</p>
-            <ul>
-                <li><OuterLink content="Node JS" destination="https://nodejs.org/en/about" /></li>
-                <li><OuterLink content="Express JS" destination="https://expressjs.com/" /></li>
-                <li><OuterLink content="React JS" destination="https://reactjs.org/" /></li>
-                <li><OuterLink content="Python" destination="https://www.python.org/about/" /></li>
-                <li><OuterLink content="MongoDB" destination="https://www.mongodb.com/" /></li>
-                <li><OuterLink content="MySQL" destination="https://www.mysql.com/" /></li>
-                <li><OuterLink content="Unity Game Engine" destination="https://unity.com/" /></li>
-            </ul>
-            <p>Here is my favorite, Unusual but effective stack for Client-sided web development.</p>
-            <ul>
-                <li>
-                    React JS
-                </li>
-                <li>
-                    MongoDB
-                </li>
-                <li>
-                    .Net Core web API
-                </li>
-            </ul>
+            <div className="about-desc">
+                <p>Hmm, where should I begin…</p>
+                <p>I'm proficient in <strong style={{ "color": "#96e2f0" }}>C#</strong> and the{' '}
 
-            <p>I know i know... People prefer Node JS with Express.</p>
-            <p>But The reason you see .NET Core web API in there is because currently it is the fastest out there in terms of performance, and obviously C# though!</p>
-            <p>But if we were to talk in terms of popular stacks, I'd say I'm proficient in using the <strong>MERN</strong> stack.</p>
-            <p>If I were to develop Server-side Web Apps, I'd most likely go with this bad boy right here: </p>
-            <div className="img-container">
-                <OuterLink content={<img className="nextjs-logo" src="/images/next.png" alt="Next JS logo" />} destination="https://nextjs.org/" />
+                <OuterLink
+                content=".Net Framework"
+                destination="https://dotnet.microsoft.com/en-us/learn/dotnet/what-is-dotnet-framework" />
+
+                {' '}(Obviously) but what would I be if it was all I know, <i>eh?</i></p>
+                <p>Well, here is a list of the other technologies I know how to use:</p>
+                <ul>
+                    <li><OuterLink content="Node JS" destination="https://nodejs.org/en/about" /></li>
+                    <li><OuterLink content="Express JS" destination="https://expressjs.com/" /></li>
+                    <li><OuterLink content="React JS" destination="https://reactjs.org/" /></li>
+                    <li><OuterLink content="Python" destination="https://www.python.org/about/" /></li>
+                    <li><OuterLink content="MongoDB" destination="https://www.mongodb.com/" /></li>
+                    <li><OuterLink content="MySQL" destination="https://www.mysql.com/" /></li>
+                    <li><OuterLink content="Unity Game Engine" destination="https://unity.com/" /></li>
+                </ul>
+                <p>Here is my favorite, Unusual but effective stack for Client-sided web development.</p>
+                <ul>
+                    <li>
+                        React JS
+                    </li>
+                    <li>
+                        MongoDB
+                    </li>
+                    <li>
+                        .Net Core web API
+                    </li>
+                </ul>
+
+                <p><i>I know i know...</i> People prefer Node JS with Express.</p>
+                <p>But The reason you see .NET Core web API in there is because currently it is the fastest out there in terms of performance, and obviously C# though!</p>
+                <p>But if we were to talk in terms of popular stacks, I'd say I'm proficient in using the <strong>MERN</strong> stack.</p>
+                <p>If I were to develop Server-side Web Apps, I'd most likely go with this bad boy right here: </p>
+                <div className="img-container">
+                    <OuterLink content={<img className="nextjs-logo" src="/images/next.png" alt="Next JS logo" />} destination="https://nextjs.org/" />
+                </div>
             </div>
         </section>
     );
--- src/components/AboutSections/OtherSkillsSection.js
+++ src/components/AboutSections/OtherSkillsSection.js
@@ -1,28 +1,33 @@
+import OuterLink from "../OuterLink";
+
 export default function OtherSkillsSection() {
     return (
         <section className="about-section dark-blue-bg">
             <h1 className="about-header">Other Skills</h1>
-            <p>Getting right to the point, here is yet another list of the things I can do</p>
-            <ul>
-                <li>
-                    Assemble computer hardware
-                </li>
-                <li>
-                    Git and version control
-                </li>
-                <li>
-                    Docker
-                </li>
-                <li>
-                    Bash & PowerShell
-                </li>
-                <li>
-                    Ubuntu OS
-                </li>
-            </ul>
-            <p>During my path it was rather natural for me to pick up the more modern skills that go with software development,
-                in order to both make my life easier and speed up my development pace.</p>
-            <p>As for the Assembly of computer hardware, this was just me and my curiosity.</p>
+            <div className="about-desc">
+                <p>Getting right to the point, here is yet another list of the things I can do</p>
+                <ul>
+                    <li>
+                        Assemble computer hardware
+                    </li>
+                    <li>
+                        <OuterLink content="Git and version control" destination="https://git-scm.com/" />
+                    </li>
+                    <li>
+                        <OuterLink content="Docker" destination="https://www.docker.com/" />
+                    </li>
+                    <li>
+                        <OuterLink content="Bash" destination="https://en.wikipedia.org/wiki/Bash_(Unix_shell)" /> & {" "}
+                        <OuterLink content="PowerShell" destination="https://learn.microsoft.com/en-us/powershell/scripting/overview?view=powershell-7.3" />
+                    </li>
+                    <li>
+                        <OuterLink content="Ubuntu OS" destination="https://ubuntu.com/desktop" />
+                    </li>
+                </ul>
+                <p>During my path it was rather natural for me to pick up the more modern skills that go with software development,
+                    in order to both make my life easier and speed up my development pace.</p>
+                <p>As for the Assembly of computer hardware, this was just me and my curiosity.</p>
+            </div>
         </section>
     );
 }
