9a4c8339b111b15ac59635ac88373f85c247b73f
Tenmayos <tenmayos_2@hotmail.com>
2023-02-10 01:51:44 +0300

    Replaced `Navbar` with `Layout` Component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import ContactMe from "./pages/ContactMe";
-import Navbar from "./components/Navbar";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<ContactMe />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<ContactMe />} />
+        </Routes>
+      </Layout>
     </>
   );
 }
