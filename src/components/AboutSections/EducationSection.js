5cbabc1d9cf4fff0b38c35d3b308293ee532baa2
Tenmayos <tenmayos_2@hotmail.com>
2023-02-10 01:55:28 +0300

    Wrapped `EducationSection` description in a div

--- src/components/AboutSections/EducationSection.js
+++ src/components/AboutSections/EducationSection.js
@@ -5,14 +5,16 @@ export default function EducationSection() {
     return (
         <section className="about-section dark-blue-bg">
             <h1 className="section-header">Education</h1>
-            <p className="center"><u>Oct 2018 - May 2021</u></p>
-            <p className="center">Bachelor's Degree in Management Information Systems <strong>(MIS)</strong></p>
-            <p className="center">Alexandria University, Alexandria <strong>(Egypt)</strong></p>
-            <div className="img-container">
+            <div className="about-desc">
+                <p className="center"><u>Oct 2018 - May 2021</u></p>
+                <p className="center">Bachelor's Degree in Management Information Systems <strong>(MIS)</strong></p>
+                <p className="center">Alexandria University, Alexandria <strong>(Egypt)</strong></p>
+                <div className="img-container">
 
-                <OuterLink
-                    content={<img className="alex-logo" src="images/AlexUni_logo.png" alt="University's logo" />}
-                    destination="https://alexu.edu.eg/index.php/en/" />
+                    <OuterLink
+                        content={<img className="alex-logo" src="images/AlexUni_logo.png" alt="University's logo" />}
+                        destination="https://alexu.edu.eg/index.php/en/" />
+                </div>
             </div>
         </section>
     );
