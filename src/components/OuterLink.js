6ce0ffc3225cb462b351c312b49af1bf742373e5
Tenmayos <tenmayos_2@hotmail.com>
2023-02-10 01:54:30 +0300

    Added dynamic styling for clicking on `OuterLink`

--- src/components/OuterLink.js
+++ src/components/OuterLink.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 /**
  * 
  * @param {{destination: string, className: string, content: any}} props 
  */
 export default function OuterLink(props) {
 
-    return <a className={props.className}
-    href={props.destination}
-    target="_blank"
-    rel="noreferrer noopener">
-    {props.content}
+    const [isClicked, setIsClicked] = useState(false);
+    // Clicking something on phone should light up to cyan until the user is back.
+
+    return <a
+        style={isClicked ? { "color": "cyan" } : null}
+        className={props.className}
+        href={props.destination}
+        onMouseDown={() => setIsClicked(true)}
+        onMouseLeave={() => setIsClicked(false)}
+        target="_blank"
+        rel="noreferrer noopener">
+        {props.content}
     </a>;
 }
