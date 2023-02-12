d380cae3dad77767f8738cde1a2bc2d591f8e610
Tenmayos <tenmayos_2@hotmail.com>
2023-02-12 17:10:44 +0300

    Added experimental icons to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
+import { useLocation } from "react-router-dom";
+
 export default function Footer() {
-    
+
+    if (useLocation().pathname === "/")
+        return null;
+
     const startYear = 2023;
     const currentYear = new Date().getFullYear();
 
     return (
         <section className="footer-section">
-        <span>© Bassam Saleh {startYear}-{currentYear}</span>
+            <p>© Bassam Saleh {startYear}-{currentYear}</p>
+            <i className="fa-brands fa-discord"></i>
+            <i className="fa-brands fa-github"></i>
+            <i className="fa-brands fa-linkedin"></i>
         </section>
     );
 }
