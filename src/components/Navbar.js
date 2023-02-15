fcfa142050dc15f8a423d9c49cf459d50ea4fd1f
Tenmayos <tenmayos_2@hotmail.com>
2023-02-15 20:15:09 +0300

    Removed Location checking from `Navbar`

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,7 @@
 import "../styles/App.css"
 import LinksList from "./LinksList"
-import { useLocation } from "react-router-dom"
 
 export default function Navbar() {
-
-    const location = useLocation();
-
-    if (location.pathname === "/") {
-        return null;
-    }
 
     return (
         <div className="topnav">
