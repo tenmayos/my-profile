9ac3668e501e81b4e379e4f0f66f0f1facfba50a
Tenmayos <tenmayos_2@hotmail.com>
2023-02-15 20:14:17 +0300

    Removed location checking Wrapped fa-icons in an `OuterLink`

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,27 @@
-import { useLocation } from "react-router-dom";
+import OuterLink from "./OuterLink";
+import "../styles/App.css";
+import "../styles/Footer.css";
 
 export default function Footer() {
-
-    if (useLocation().pathname === "/")
-        return null;
 
     const startYear = 2023;
     const currentYear = new Date().getFullYear();
 
     return (
-        <section className="footer-section">
+        <footer className="footer-section">
             <p>© Bassam Saleh {startYear}-{currentYear}</p>
-            <i className="fa-brands fa-discord"></i>
-            <i className="fa-brands fa-github"></i>
-            <i className="fa-brands fa-linkedin"></i>
-        </section>
+
+            {/* Discord add link */}
+            <OuterLink destination="https://discordapp.com/users/165250441230155776" className="clickable"
+            content=<i className="fa-brands fa-discord fa-lg margin"></i> />
+            
+            {/* Github profile link */}
+            <OuterLink destination="https://github.com/tenmayos" className="clickable"
+            content=<i className="fa-brands fa-github fa-lg margin"></i> />
+            
+            {/* LinkedIn profile link */}
+            <OuterLink destination="https://www.linkedin.com/in/bassam-al-nezi-520940164/" className="clickable"
+            content=<i className="fa-brands fa-linkedin fa-lg margin"></i> />
+        </footer>
     );
 }
